e647338f9a4a205a6f37cea5c4607bf803a09767
Jaime <jaijimri@gmail.com>
2021-10-04 18:52:01 +0200

    Primera Prueba de Clase Playlist Terminada

--- trianafyG9/src/main/java/com/salesianostriana/dam/trianafyG9/controller/PlaylistController.java
+++ trianafyG9/src/main/java/com/salesianostriana/dam/trianafyG9/controller/PlaylistController.java
@@ -1,4 +1,62 @@
 package com.salesianostriana.dam.trianafyG9.controller;
 
+import com.salesianostriana.dam.trianafyG9.model.Playlist;
+import com.salesianostriana.dam.trianafyG9.model.PlaylistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lists")
+@RequiredArgsConstructor
 public class PlaylistController {
+
+    private final PlaylistRepository playlistRepository;
+
+    //Listar todos
+    @GetMapping("")
+    public ResponseEntity<List<Playlist>> findAll(){
+        return ResponseEntity.ok().body(playlistRepository.findAll());
+
+    }
+
+    //Listar uno solo por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Playlist> findOne(@PathVariable("id") Long id){
+        return ResponseEntity.of(playlistRepository.findById(id));
+    }
+
+    //Crear nueva playlist
+    @PostMapping("")
+    public ResponseEntity<Playlist> create(@RequestBody Playlist playlist){
+        return ResponseEntity.status(201).body(playlistRepository.save(playlist));
+    }
+
+    //Modificar Playlist
+    @PutMapping("/{id}")
+    public  ResponseEntity<Playlist> edit(@RequestBody Playlist playlist, @PathVariable Long id){
+        return ResponseEntity.of(
+                playlistRepository.findById(id).map(list -> {
+                  list.setName(playlist.getName());
+                  list.setDescripcion(playlist.getDescripcion());
+                  list.setSongs(playlist.getSongs());
+                  playlistRepository.save(list);
+                  return list;
+                })
+        );
+    }
+
+    //Borrar una Playlist
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+
+        playlistRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+
+
 }
---
+++ trianafyG9/src/main/java/com/salesianostriana/dam/trianafyG9/dto/PlaylistDtoConverter.java
@@ -0,0 +1,29 @@
+package com.salesianostriana.dam.trianafyG9.dto;
+
+import com.salesianostriana.dam.trianafyG9.model.Playlist;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlaylistDtoConverter {
+
+    public Playlist createPlaylistDtoToPlaylist(CreatePlaylistDto c){
+        return new Playlist(
+                c.getName(),
+                c.getDescription(),
+                c.getSongId()
+        );
+    }
+
+    public GetPlaylistDto playlistToGetPlaylistDto(Playlist p){
+
+        GetPlaylistDto result = new GetPlaylistDto();
+
+        result.setName(p.getName());
+        result.setDescription(p.getDescripcion());
+        result.setSong(p.getSongs().getTitle());
+
+        return result;
+
+    }
+
+}
