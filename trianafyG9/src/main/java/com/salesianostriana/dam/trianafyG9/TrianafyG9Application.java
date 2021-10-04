bda224abe2992f46a2246c53a075123f6822cc4d
Daniel Oliva García <oliva.gadan21@triana.salesianos.edu>
2021-10-04 10:09:22 +0200

    Proyecto creado

---
+++ trianafyG9/src/main/java/com/salesianostriana/dam/trianafyG9/TrianafyG9Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.trianafyG9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianafyG9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianafyG9Application.class, args);
+	}
+
+}
---
+++ trianafyG9/src/test/java/com/salesianostriana/dam/trianafyG9/TrianafyG9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.trianafyG9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrianafyG9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
